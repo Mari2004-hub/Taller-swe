618d8780207cd0113223ba5953c7724f997c8059
m.murciam <m.murciam@uniandes.edu.co>
2026-04-20 16:23:41 -0500

    12

--- mynewapp/src/app/series/series.service.ts
+++ mynewapp/src/app/series/series.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
 import { Observable } from 'rxjs';
 import { Serie } from './serie';
 
@@ -8,7 +9,7 @@ import { Serie } from './serie';
 })
 export class SeriesService {
 
-  private apiUrl: string = 'https://gist.githubusercontent.com/josejbocanegra/8490b48961a69dcd2bfd8a360256d0db/raw/34ff30dbc32392a69eb0e08453a3fc975a3890f0/series.json';
+  private apiUrl: string = environment.baseUrl + 'series.json';
 
   constructor(private http: HttpClient) {}
 
